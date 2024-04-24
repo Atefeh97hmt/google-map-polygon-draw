9d5df1994d7a05c3a22bbbc7327ec008a1cae203
Atefeh97hmt <hemmati.atefeh97@gmail.com>
2024-04-24 11:58:18 +0330

    shiraz map added

--- polygon-draw.js
+++ polygon-draw.js
@@ -1,9 +1,10 @@
 var map;
 var drawingManager;
+var drawnPolygon;
 
 function initialize() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 24.8998373, lng: 91.8258764 },
+        center: { lat: 29.5916, lng: 52.5836 }, // Coordinates for Shiraz, Iran
         zoom: 12,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     });
@@ -24,6 +25,39 @@ function initialize() {
     });
 
     drawingManager.setMap(map);
+
+    // Add event listener for polygon complete
+    google.maps.event.addListener(drawingManager, 'overlaycomplete', function(event) {
+        if (event.type == google.maps.drawing.OverlayType.POLYGON) {
+            // Show the share link
+            var shareLink = document.getElementById('shareLink');
+            shareLink.style.display = 'block';
+            // Store the drawn polygon
+            drawnPolygon = event.overlay;
+            // Create link to the map with the polygon's coordinates
+            var polygonBounds = event.overlay.getPath().getArray();
+            var polygonCoords = [];
+            for (var i = 0; i < polygonBounds.length; i++) {
+                polygonCoords.push(polygonBounds[i].lat() + ',' + polygonBounds[i].lng());
+            }
+            var mapLink = 'https://www.google.com/maps/dir//' + polygonCoords.join('/');
+            shareLink.href = mapLink;
+        }
+    });
+
+    // Add event listener for polygon editing
+    google.maps.event.addListener(drawingManager, 'overlaycomplete', function(event) {
+        if (event.type == google.maps.drawing.OverlayType.POLYGON) {
+            // Update the share link when the polygon is edited
+            var polygonBounds = event.overlay.getPath().getArray();
+            var polygonCoords = [];
+            for (var i = 0; i < polygonBounds.length; i++) {
+                polygonCoords.push(polygonBounds[i].lat() + ',' + polygonBounds[i].lng());
+            }
+            var mapLink = 'https://www.google.com/maps/dir//' + polygonCoords.join('/');
+            document.getElementById('shareLink').href = mapLink;
+        }
+    });
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
